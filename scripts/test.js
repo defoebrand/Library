2feae255cfbaf469ce9b09d51d20fc623a2d99f2
uimarshall <uimarshall@gmail.com>
2020-10-29 14:57:52 +0100

    Add button to show and hide form

--- scripts/test.js
+++ scripts/test.js
@@ -1,25 +1,44 @@
 let myLibrary = []
 
 const bookShelf = document.querySelector('.bookShelf');
+// Get a form with an 'id' of add-book
+const addForm = document.forms['add-book']
 
 const newBookButton = document.getElementById('newBookButton');
-newBookButton.onclick = () => {
-  newBookButton.appendChild(newBookForm);
-}
-let newBookForm = document.createElement('form')
-let titleInput = document.createElement('input');
-titleInput.type = 'text'
-newBookForm.appendChild(titleInput);
-let authorInput = document.createElement('input');
-authorInput.type = 'text'
-newBookForm.appendChild(authorInput);
-let pagesInput = document.createElement('input');
-pagesInput.type = 'text'
-newBookForm.appendChild(pagesInput);
-let readInput = document.createElement('input');
-readInput.type = 'text'
-newBookForm.appendChild(readInput);
+console.log(newBookButton)
+console.log(addForm)
+
+newBookButton.addEventListener('click', (e)=>{
+  console.log(e.target)
+  if (newBookButton.click) {
+    // alert('i am clicked')
+     addForm.style.display = "none"
+    
+  }else{
+   addForm.style.display = "initial"
+ }
+});
+
+
+// newBookButton.onclick = () => {
+//   newBookButton.appendChild(newBookForm);
+// }
+// let newBookForm = document.createElement('form')
+// let titleInput = document.createElement('input');
+// titleInput.type = 'text'
+// newBookForm.appendChild(titleInput);
+// let authorInput = document.createElement('input');
+// authorInput.type = 'text'
+// newBookForm.appendChild(authorInput);
+// let pagesInput = document.createElement('input');
+// pagesInput.type = 'text'
+// newBookForm.appendChild(pagesInput);
+// let readInput = document.createElement('input');
+// readInput.type = 'text'
+// newBookForm.appendChild(readInput);
 // newBookButton.appendChild(titleInput);
+
+
 
 function Book(title, author, pages, read) {
   this.title = title
