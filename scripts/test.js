5400d4765c271c692f86fb50c320784295d58433
defoebrand <defoe.brand@gmail.com>
2020-10-29 17:07:06 +0200

    Finish functioning of form by adding books to shelf and having checkbox

--- scripts/test.js
+++ scripts/test.js
@@ -1,52 +1,39 @@
 let myLibrary = []
 
 const bookShelf = document.querySelector('.bookShelf');
-// Get a form with an 'id' of add-book
-const addForm = document.forms['add-book']
+
+const addForm = document.querySelector('.add-book');
+
+const submitForm = document.querySelector('.submitForm')
+submitForm.onclick = () => {
+  title = addForm.firstElementChild.value;
+  author = addForm.firstElementChild.nextElementSibling.value;
+  pages = addForm.firstElementChild.nextElementSibling.nextElementSibling.value;
+  read = checkIfRead();
+  addBookToLibrary(title, author, pages, read)
+  addBookToShelf();
+}
 
 const newBookButton = document.getElementById('newBookButton');
-console.log(newBookButton)
-console.log(addForm)
 
-newBookButton.addEventListener('click', (e)=>{
-  console.log(e.target)
-  if (newBookButton.click) {
-    // alert('i am clicked')
-     addForm.style.display = "none"
-    
-  }else{
-   addForm.style.display = "initial"
- }
-});
-
-
-// newBookButton.onclick = () => {
-//   newBookButton.appendChild(newBookForm);
-// }
-// let newBookForm = document.createElement('form')
-// let titleInput = document.createElement('input');
-// titleInput.type = 'text'
-// newBookForm.appendChild(titleInput);
-// let authorInput = document.createElement('input');
-// authorInput.type = 'text'
-// newBookForm.appendChild(authorInput);
-// let pagesInput = document.createElement('input');
-// pagesInput.type = 'text'
-// newBookForm.appendChild(pagesInput);
-// let readInput = document.createElement('input');
-// readInput.type = 'text'
-// newBookForm.appendChild(readInput);
-// newBookButton.appendChild(titleInput);
-
-
+newBookButton.onclick = () => {
+  addForm.classList.toggle('hide');
+};
 
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
-  this.read = read
   this.info = function() {
     return (title + " by " + author + ", " + pages + " pages, " + read)
+  }
+}
+
+function checkIfRead() {
+  if (addForm.lastElementChild.previousElementSibling.lastElementChild.checked == true) {
+    return 'Read It'
+  } else {
+    return "Haven't Read It"
   }
 }
 
@@ -55,8 +42,14 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(newBook);
 }
 
+function addBookToShelf() {
+  let bookToShelve = document.createElement('li');
+  bookToShelve.innerText = myLibrary[myLibrary.length - 1].info();
+  bookShelf.appendChild(bookToShelve)
+}
+
 function putBooksOnTheShelf() {
-  for (let i = 0; i <= myLibrary.length; i++) {
+  for (let i = 0; i <= myLibrary.length - 1; i++) {
     let shelvedBook = document.createElement('li');
     shelvedBook.innerText = myLibrary[i].info();
     bookShelf.appendChild(shelvedBook);
@@ -66,7 +59,7 @@ function putBooksOnTheShelf() {
 
 
 
-addBookToLibrary('Test_title', 'Me', 213, 'read it');
-addBookToLibrary('Test_title2', 'Me', 213, 'read it');
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+addBookToLibrary('Test_title', 'Me', 213, 'read');
+addBookToLibrary('Test_title2', 'Me', 213, 'read');
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
 putBooksOnTheShelf();
