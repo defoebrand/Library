ea9eb30ff6fcc7ef03c97bdc23bf073b6a521779
defoebrand <defoe.brand@gmail.com>
2020-10-30 17:52:34 +0200

    Adjust for linter errors

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
-let myLibrary = []
+const myLibrary = [];
 
 if (localStorage.myLibrary) {
-  let storage = JSON.parse(localStorage.myLibrary)
-  let length = storage.length;
-  for (let i = 0; i <= length - 1; i++) {
+  const storage = JSON.parse(localStorage.myLibrary);
+  const {
+    length,
+  } = storage;
+  for (let i = 0; i <= length - 1; i += 1) {
     myLibrary[i] = storage[i];
   }
 }
@@ -17,56 +19,47 @@ newBookButton.onclick = () => {
   addForm.classList.toggle('show');
 };
 
-const submitForm = document.querySelector('.submitForm')
-submitForm.onclick = () => {
-  title = addForm.firstElementChild.value;
-  author = addForm.firstElementChild.nextElementSibling.value;
-  pages = addForm.firstElementChild.nextElementSibling.nextElementSibling.value;
-  read = checkIfRead(addForm.lastElementChild.previousElementSibling.lastElementChild.checked);
-  addBookToLibrary(title, author, pages, read)
-  shelveTheBook(myLibrary.length - 1);
-}
+const submitForm = document.querySelector('.submitForm');
 
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function checkIfRead(checked) {
-  if (checked == true) {
-    return 'Read It'
-  } else {
-    return "Haven't Read It"
+  if (checked === true) {
+    return 'Read It';
   }
+  return "Haven't Read It";
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  let newBook = new Book(title, author, pages, read);
-  newBook.index = myLibrary.length
+  const newBook = new Book(title, author, pages, read);
+  newBook.index = myLibrary.length;
   myLibrary.push(newBook);
-  localStorage.myLibrary = JSON.stringify(myLibrary)
+  localStorage.myLibrary = JSON.stringify(myLibrary);
 }
 
 function shelveTheBook(spot) {
-  let bookToShelve = document.createElement('li');
-  bookToShelve.innerText = myLibrary[spot].title + " by " + myLibrary[spot].author + ", " + myLibrary[spot].pages + " pages "
+  const bookToShelve = document.createElement('li');
+  bookToShelve.innerText = `${myLibrary[spot].title} by ${myLibrary[spot].author}, ${myLibrary[spot].pages} pages `;
 
-  let deleteBook = document.createElement('span');
-  deleteBook.innerText = " - Delete From Library - "
-  deleteBook.onclick = function() {
+  const deleteBook = document.createElement('span');
+  deleteBook.innerText = ' - Delete From Library - ';
+  deleteBook.onclick = () => {
     myLibrary.splice(bookToShelve.index, 1);
     bookShelf.removeChild(bookToShelve);
-  }
+  };
 
-  let readBox = document.createElement('input')
-  readBox.type = 'checkbox'
+  const readBox = document.createElement('input');
+  readBox.type = 'checkbox';
 
-  let readBook = document.createElement('span');
+  const readBook = document.createElement('span');
   readBook.innerText = myLibrary[spot].read;
 
-  if (readBook.innerText == "Read It") {
+  if (readBook.innerText === 'Read It') {
     readBook.classList.add('text-green');
     readBox.checked = true;
   } else {
@@ -75,26 +68,35 @@ function shelveTheBook(spot) {
   }
 
   readBox.onchange = () => {
-    if (readBox.checked == true) {
-      readBook.innerText = "Read It"
-      readBook.classList.remove('text-red')
-      readBook.classList.add('text-green')
+    if (readBox.checked === true) {
+      readBook.innerText = 'Read It';
+      readBook.classList.remove('text-red');
+      readBook.classList.add('text-green');
     } else {
-      readBook.innerText = "Haven't Read It"
-      readBook.classList.remove('text-green')
-      readBook.classList.add('text-red')
+      readBook.innerText = "Haven't Read It";
+      readBook.classList.remove('text-green');
+      readBook.classList.add('text-red');
     }
-  }
+  };
 
   bookToShelve.appendChild(deleteBook);
   bookToShelve.appendChild(readBox);
   bookToShelve.appendChild(readBook);
   bookShelf.appendChild(bookToShelve);
-
 }
 
+submitForm.onclick = () => {
+  const title = addForm.firstElementChild.value;
+  const author = addForm.firstElementChild.nextElementSibling.value;
+  const pages = addForm.firstElementChild.nextElementSibling.nextElementSibling.value;
+  const checkbox = addForm.lastElementChild.previousElementSibling.lastElementChild;
+  const read = checkIfRead(checkbox.checked);
+  addBookToLibrary(title, author, pages, read);
+  shelveTheBook(myLibrary.length - 1);
+};
+
 function putBooksOnTheShelf() {
-  for (let i = 0; i <= myLibrary.length - 1; i++) {
+  for (let i = 0; i <= myLibrary.length - 1; i += 1) {
     shelveTheBook(i);
   }
 }
